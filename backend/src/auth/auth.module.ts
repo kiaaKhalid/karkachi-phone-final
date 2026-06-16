11a9e959a00280fa381dd7f6f9459909339758e4
Khalid kiaa <kiaakhalid4@gmail.com>
2026-06-16 09:03:18 +0100

    feat(auth): setup auth module configuration

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthController } from './auth.controller.js';
+import { AuthService } from './auth.service.js';
+import { CryptoService } from './crypto.service.js';
+import { UsersModule } from '../users/users.module.js';
+import { JwtStrategy } from './jwt.strategy.js';
+import { JwtRefreshStrategy } from './jwt-refresh.strategy.js';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN', '1d') as any,
+        },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, CryptoService, JwtStrategy, JwtRefreshStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
