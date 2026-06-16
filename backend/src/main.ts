670c8bf0370ed10dd261c0960a56205f3d34096c
Khalid kiaa <kiaakhalid4@gmail.com>
2026-06-16 09:03:21 +0100

    feat(main): add main logic

---
+++ backend/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module.js';
+import { json, urlencoded } from 'express';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
